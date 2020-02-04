c7502de1f74b4055cc5647cf28e70f3f66cd303e
Mirek Zvolský <zvolsky@seznam.cz>
2020-02-04 11:11:28 +0100

    functionality ready for django basic auth_user and pg_dump

--- pg_dump_anonymized/anonymized_dump.py
+++ pg_dump_anonymized/anonymized_dump.py
@@ -5,11 +5,14 @@
 #    psql -d <newdb> < /home/mirek/dj/opentrafficweb/opentrafficweb/opentrafficweb-anonymized.sql
 
 import logging
+import os
 import subprocess
+import sys
 
 from faker import Faker
 
 
+DUMPPATH = os.path.join(os.path.split(os.path.abspath(sys.argv[0]))[0], "dump")  # <project_root>/dump
 MODIFIER = "_aa_aa_aa_aa_aa_aa_aa_aa_aa_aa"
 FORMAT = '%(asctime) %(message)s'  # this is not necessary because it is a default
 logging.basicConfig(format=FORMAT)
@@ -29,48 +32,70 @@ class Dump:
             self.dump_5_cleanup(cursor)
 
     def dump_1_create_table_copies(self, cursor):
+        # create table copies for tables which need anonymize
         self.dump_5_cleanup(cursor)
         cursor.execute("create table auth_user%s (like auth_user including all)" % MODIFIER)
         cursor.execute("insert into auth_user%s select * from auth_user" % MODIFIER)
 
     def dump_2_anonymize_tables(self, cursor):
+        # generate faked content into table copies
         fake = Faker()
 
         cursor.execute("SELECT id, is_superuser, username FROM auth_user")
         users = cursor.fetchall()
 
+        retries = 0
         for user_id, is_superuser, username in users:
             fd1 = fake.date()
             fd2 = fake.date()
-            cursor.execute("UPDATE auth_user" + MODIFIER +
-                           " SET password = '', email=%s, first_name = %s, last_name = %s, "
-                           "date_joined = %s, "
-                           "last_login = %s, "
-                           "is_superuser = false, is_staff = false, username = %s "
-                           "WHERE id = %s", [fake.email(), fake.first_name(), fake.last_name(),
-                                             '%s %s' % (min(fd1, fd2), fake.time()),
-                                             '%s %s' % (max(fd1, fd2), fake.time()),
-                                             ('user_%s' % user_id),  # if is_superuser else username,
-                                             user_id])
+            # this is very stupid retry in case of duplicities in unique fields (here: email)
+            # if count of retries will grow it must be handled in a different way (ie. modify the entry, ..)
+            for _i in range(10):
+                try:
+                    cursor.execute("UPDATE auth_user" + MODIFIER +
+                                   " SET password = '', email=%s, first_name = %s, last_name = %s, "
+                                   "date_joined = %s, "
+                                   "last_login = %s, "
+                                   "is_superuser = false, is_staff = false, username = %s "
+                                   "WHERE id = %s", [fake.email(), fake.first_name(), fake.last_name(),
+                                                     '%s %s' % (min(fd1, fd2), fake.time()),
+                                                     '%s %s' % (max(fd1, fd2), fake.time()),
+                                                     ('user_%s' % user_id),  # if is_superuser else username,
+                                                     user_id])
+                    break
+                except Exception:
+                    retries += 1
+        self.logres('auth_user', len(users), retries)
 
     def dump_3_dump(self, dbname=None, username=None):
+        # dump exclude original (private) tables
         if dbname is None:
             dbname = self.dbname
         if username is None:
             username = dbname
-        cmd = ("pg_dump --dbname=%s --username=%s" % (dbname, username)).split()   # -Fc fails by restore
+        dumpcmd = "pg_dump"
+        cmd = ("%s --dbname=%s --username=%s" % (dumpcmd, dbname, username)).split()   # -Fc fails by restore
         cmd.append("--exclude-table=auth_user")
-        with open("%s-anonymized.sql" % dbname, "w") as outfile:
+        with open("%s/%s-anonymized.sql" % (DUMPPATH, dbname), "w") as outfile:
             res = subprocess.run(cmd, stdout=outfile).returncode
-        if res:
-            logging.error('pg_dump has finished with error value: %i' % res)
-        else:
-            logging.info('pg_dump has finished with no errors')
+        self.logdumpres(dumpcmd, res)
 
     def dump_4_fixdump(self, dbname=None):
+        # change table names in dump back to proper name
         if dbname is None:
             dbname = self.dbname
-        subprocess.run(("sed -i s/%s//g %s-anonymized.sql" % (MODIFIER, dbname)).split())
+        subprocess.run(("sed -i s/%s//g %s/%s-anonymized.sql" % (MODIFIER, DUMPPATH, dbname)).split())
 
     def dump_5_cleanup(self, cursor):
         cursor.execute("drop table if exists auth_user%s" % MODIFIER)
+
+    @staticmethod
+    def logres(tbl, cnt, retries):
+        logging.info('%s anonymized: %s rows, %s retries' % (tbl, cnt, retries))
+
+    @staticmethod
+    def logdumpres(dumpcmd, res):
+        if res:
+            logging.error('%s has finished with error value: %i' % (dumpcmd, res))
+        else:
+            logging.info('%s has finished with no errors', dumpcmd)
