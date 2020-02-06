d2c29d763c5f3ad1e7ef6cfd4ecdd84480e3e02e
Mirek Zvolský <zvolsky@seznam.cz>
2020-02-06 09:16:16 +0100

    removed unused script

--- scripts/anonymize.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists auth_user_aa_aa_aa_aa_aa_aa_aa_aa_aa_aa;
-create table auth_user_aa_aa_aa_aa_aa_aa_aa_aa_aa_aa (like auth_user including all);
-insert into auth_user_aa_aa_aa_aa_aa_aa_aa_aa_aa_aa select * from auth_user;
-
