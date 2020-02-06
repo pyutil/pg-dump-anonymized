5d017067482b6d9bc23f1dac9367f6ef42a317d1
Mirek Zvolský <zvolsky@seznam.cz>
2020-02-06 11:44:44 +0100

    renames

--- pg_dump_anonymized/management/commands/anonymized_dump_basic.py
+++ pg_dump_anonymized/management/commands/anonymized_dump_basic.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
 
-try:
-    from anonymized_dump import Dump
-except ImportError:
-    from pg_dump_anonymized.anonymized_dump import Dump
+
+from pg_dump_anonymized.anonymized_dump import Dump
 
 
 class Command(BaseCommand):
